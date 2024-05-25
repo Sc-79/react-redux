187dc3bdcaaea7c3cebe325305a9cbd2971f1c70
sourav <souravchongrey@gmail.com>
2024-05-25 11:26:42 +0530

    create 1st react-redux-app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Counter from "./components/Counter";
 
 function App() {
+
+
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div>
+    <Counter/>
+    </div>
   );
 }
 
